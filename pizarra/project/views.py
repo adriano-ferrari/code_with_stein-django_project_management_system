ab21c5820fb32f40b17995a0815fa5656fe128c8
adriano-ferrari <irarrefonairda@gmail.com>
2024-03-10 23:46:27 -0300

    Criada a notes para o projeto

--- pizarra/project/views.py
+++ pizarra/project/views.py
@@ -67,6 +67,9 @@ def delete(request, pk):
     return redirect('/projects/')
 
 
+# Files
+
+
 @login_required
 def upload_file(request, project_id):
     project = Project.objects.filter(created_by=request.user).get(pk=project_id)
@@ -98,3 +101,68 @@ def delete_file(request, project_id, pk):
     return redirect(f'/projects/{project_id}/')
 
 
+# Notes
+
+
+@login_required
+def add_note(request, project_id):
+    project = Project.objects.filter(created_by=request.user).get(pk=project_id)
+
+    if request.method == 'POST':
+        name = request.POST.get('name', '')
+        body = request.POST.get('body', '')
+
+        if name and body:
+            ProjectNote.objects.create(
+                project=project,
+                name=name,
+                body=body
+            )
+
+            return redirect(f'/projects/{project_id}/')
+
+    return render(request, 'project/add_note.html', {
+        'project': project
+    })
+
+
+@login_required
+def note_detail(request, project_id, pk):
+    project = Project.objects.filter(created_by=request.user).get(pk=project_id)
+    note = project.notes.get(pk=pk)
+
+    return render(request, 'project/note_detail.html', {
+        'project': project,
+        'note': note
+    })
+
+
+@login_required
+def note_edit(request, project_id, pk):
+    project = Project.objects.filter(created_by=request.user).get(pk=project_id)
+    note = project.notes.get(pk=pk)
+
+    if request.method == 'POST':
+        name = request.POST.get('name', '')
+        body = request.POST.get('body', '')
+
+        if name and body:
+            note.name = name
+            note.body = body
+            note.save()
+
+            return redirect(f'/projects/{project_id}/')
+
+    return render(request, 'project/note_edit.html', {
+        'project': project,
+        'note': note
+    })
+
+
+@login_required
+def note_delete(request, project_id, pk):
+    project = Project.objects.filter(created_by=request.user).get(pk=project_id)
+    note = project.notes.get(pk=pk)
+    note.delete()
+
+    return redirect(f'/projects/{project_id}/')
